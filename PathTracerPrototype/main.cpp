986ac15c52d4d3aaaa6afbe2d9165a97c56f0d47
Graeme White <40415739@live.napier.ac.uk>
2020-10-10 12:04:47 +0100

    Project set up
    Re-worked the default .gitignore file from GitHub to just ignore the build folder from upload.
    
    Added basic main.cpp "Hello, World" file and CMakeLists.txt for basic starting point of the project

---
+++ PathTracerPrototype/main.cpp
@@ -0,0 +1,34 @@
+/* ==================================================================
+ * SET10110 - ADVANCED GAMES ENGINEERING
+ *
+ * GRAEME B. WHITE - 40415739
+ * ==================================================================
+ * PATH-TRACING PROTOTYPE
+ *
+ * Prototype version of the path-tracer developed for SET10110 -
+ * Advanced Games Engineering.
+ *
+ * Work is based on the path tracer developed in "Ray Tracing in one
+ * Weekend" by P. Shirley - https://raytracing.github.io/
+ */
+
+ // Header files
+#include <iostream>
+
+// Namespace
+using namespace std;
+
+/*
+ * MAIN FUNCTION
+ *
+ * The first function that is invoked when the application is loaded
+ */
+int main()
+{
+	// Output message to the console
+	cout << "Hello, World!" << endl;
+
+	// Return 0 - program is finished, all is OK
+	return 0;
+
+}
